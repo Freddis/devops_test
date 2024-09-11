8453b0c8386cbc2140d5ff8bfd3d125b07369ef4
Marko Dmitrovic <dmitrovicmarko@gmail.com>
2024-09-11 09:01:58 +0200

    Updated port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AccountApiModule } from './account.api/account.api.module';
 
 async function bootstrap() {
   const accountApi = await NestFactory.create(AccountApiModule);
-  await accountApi.listen(80);
+  await accountApi.listen(3000);
 }
 bootstrap();
